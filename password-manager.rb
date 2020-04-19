3624786a3c95e953838ef07be05fc72b9c212529
M.G.T.R Manamgoda <maanafunedu@gmail.com>
2020-04-20 02:47:40 +0530

    Update password-manager.rb

--- password-manager.rb
+++ password-manager.rb
@@ -2,7 +2,7 @@ class PasswordManager < Formula
   desc "Encrypt your passwords into a file and have access with ease."
   homepage "https://github.com/ThilinaManamgoda/password-manager"
   version "v0.9.1"
-  url "https://github.com/ThilinaManamgoda/password-manager/releases/download/v0.9.0/password-manager-darwin-v0.9.0.tar.gz"
+  url "https://github.com/ThilinaManamgoda/password-manager/releases/download/v0.9.1/password-manager-darwin-v0.9.1.tar.gz"
   sha256 "15a14eac7b51b53b251b47500a4bc2bee68c8b9691c9dbc32817856fc2aa10aa"
 
   def install
