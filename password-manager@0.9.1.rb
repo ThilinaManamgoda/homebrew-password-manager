a178e46a3d252f48bf0728f97e2d4c754e1dfd8f
maanadev <maanafunedu@gmail.com>
2020-05-05 05:53:35 +0530

    Add version 0.9.2

--- password-manager@0.9.1.rb
+++ password-manager@0.9.1.rb
@@ -1,9 +1,9 @@
-class PasswordManager < Formula
+class PasswordManagerAT091 < Formula
   desc "Encrypt your passwords into a file and have access with ease."
   homepage "https://github.com/ThilinaManamgoda/password-manager"
-  version "v0.9.2"
-  url "https://github.com/ThilinaManamgoda/password-manager/releases/download/v0.9.2/password-manager-darwin-v0.9.2.tar.gz"
-  sha256 "165201e1690d05ece6fe6834a20d97eb5c804b6f0a55e348323f59d55b285955"
+  version "v0.9.1"
+  url "https://github.com/ThilinaManamgoda/password-manager/releases/download/v0.9.1/password-manager-darwin-v0.9.1.tar.gz"
+  sha256 "15a14eac7b51b53b251b47500a4bc2bee68c8b9691c9dbc32817856fc2aa10aa"
 
   def install
     bin.install "password-manager"
